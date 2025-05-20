9bc798a13140f2afa76fb9dcf01632fb1929ac83
abisheksivaraj <abisheksivaraj2001.com>
2025-05-20 12:56:54 +0530

    add

--- frontend/src/apiConfig.js
+++ frontend/src/apiConfig.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
-export const API_URL = "http://localhost:2222";
+// export const API_URL = "http://localhost:2222";
 
-// export const API_URL = "https://project-abb-vwxt.onrender.com";
+export const API_URL = "https://project-abb-vwxt.onrender.com";
 
 export const api = axios.create({
   baseURL: API_URL,
