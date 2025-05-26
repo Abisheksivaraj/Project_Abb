50b9ba6ca5f3b90a3c179d84a5509ea8f34cec86
abisheksivaraj <abisheksivaraj2001.com>
2025-05-26 18:41:51 +0530

    add

--- frontend/src/Components/MainPage.jsx
+++ frontend/src/Components/MainPage.jsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 import { useEffect } from "react";
+import { useNavigate } from "react-router-dom"; // Add this import
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
@@ -15,28 +16,12 @@ import LogoutIcon from "@mui/icons-material/Logout";
 import logo from "../abb.svg";
 import MainPageTable from "./MainPageTable";
 
-
-const settings = [
-  {
-    label: "Logout",
-    icon: <LogoutIcon fontSize="small" />,
-    action: () => {
-      console.log("Logout clicked");
-      // Clear any authentication tokens
-      localStorage.removeItem("token");
-      // Close the profile menu before navigation
-      handleProfileMenuClose();
-      // Navigate to home page
-      navigate("/");
-    },
-  },
-];
-
 function MainPage() {
   const [anchorElUser, setAnchorElUser] = React.useState(null);
-  const [logoLoaded, setLogoLoaded] = React.useState(false);
 
-  // Check if logo is loaded correctly
+  const navigate = useNavigate();
+
+
   useEffect(() => {
     const img = new Image();
     img.src = logo;
@@ -59,87 +44,93 @@ function MainPage() {
 
   const handleLogout = () => {
     console.log("Logging out...");
-    handleProfileMenuClose();
+    handleCloseUserMenu();
     navigate("/");
   };
 
+  const settings = [
+    {
+      label: "Logout",
+      icon: <LogoutIcon fontSize="small" />,
+      action: handleLogout,
+    },
+  ];
+
   return (
-    <AppBar position="static" sx={{ bgcolor: "white", boxShadow: 2 }}>
-      <Container maxWidth="xl">
-        <Toolbar disableGutters>
-          {/* Logo for larger screens */}
-          <Box
-            sx={{ display: { xs: "none", md: "flex" }, mr: 2, flexShrink: 0 }}
-          >
-            <img
-              src={logo}
-              alt="ABB Logo"
-              style={{ height: "40px", width: "auto", display: "block" }}
-            />
-          </Box>
-
-          {/* Logo for mobile screens */}
-          <Box
-            sx={{
-              flexGrow: 1,
-              display: { xs: "flex", md: "none" },
-              justifyContent: "between",
-            }}
-          >
-            <img
-              src={logo}
-              alt="ABB Logo"
-              style={{ height: "35px", width: "auto", display: "block" }}
-            />
-          </Box>
-
-          {/* Empty space to push logo to left and logout to right on desktop */}
-          <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }} />
-
-          {/* User avatar and menu */}
-          <Box sx={{ flexGrow: 0, ml: 2 }}>
-            <Tooltip title="User settings">
-              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                <Avatar alt="User" src="/static/images/avatar/2.jpg" />
-              </IconButton>
-            </Tooltip>
-            <Menu
-              sx={{ mt: "45px" }}
-              id="menu-appbar"
-              anchorEl={anchorElUser}
-              anchorOrigin={{
-                vertical: "top",
-                horizontal: "right",
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: "top",
-                horizontal: "right",
-              }}
-              open={Boolean(anchorElUser)}
-              onClose={handleCloseUserMenu}
+    <>
+      <AppBar position="static" sx={{ bgcolor: "white", boxShadow: 2 }}>
+        <Container maxWidth="xl">
+          <Toolbar disableGutters>
+            <Box
+              sx={{ display: { xs: "none", md: "flex" }, mr: 2, flexShrink: 0 }}
             >
-              {settings.map((setting) => (
-                <MenuItem
-                  key={setting.label}
-                  onClick={() => {
-                    setting.action();
-                    handleCloseUserMenu();
-                  }}
-                >
-                  <Box sx={{ display: "flex", alignItems: "center", gap: 1 }}>
-                    {setting.icon}
-                    <Typography>{setting.label}</Typography>
-                  </Box>
-                </MenuItem>
-              ))}
-            </Menu>
-          </Box>
-        </Toolbar>
-        
-      </Container>
-      
-    </AppBar>
+              <img
+                src={logo}
+                alt="ABB Logo"
+                style={{ height: "40px", width: "auto", display: "block" }}
+              />
+            </Box>
+
+            <Box
+              sx={{
+                flexGrow: 1,
+                display: { xs: "flex", md: "none" },
+                justifyContent: "between",
+              }}
+            >
+              <img
+                src={logo}
+                alt="ABB Logo"
+                style={{ height: "35px", width: "auto", display: "block" }}
+              />
+            </Box>
+
+            <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }} />
+
+            <Box sx={{ flexGrow: 0, ml: 2 }}>
+              <Tooltip title="User settings">
+                <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                  <Avatar alt="User" src="/static/images/avatar/2.jpg" />
+                </IconButton>
+              </Tooltip>
+              <Menu
+                sx={{ mt: "45px" }}
+                id="menu-appbar"
+                anchorEl={anchorElUser}
+                anchorOrigin={{
+                  vertical: "top",
+                  horizontal: "right",
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: "top",
+                  horizontal: "right",
+                }}
+                open={Boolean(anchorElUser)}
+                onClose={handleCloseUserMenu}
+              >
+                {settings.map((setting) => (
+                  <MenuItem
+                    key={setting.label}
+                    onClick={() => {
+                      setting.action();
+                      handleCloseUserMenu();
+                    }}
+                  >
+                    <Box sx={{ display: "flex", alignItems: "center", gap: 1 }}>
+                      {setting.icon}
+                      <Typography>{setting.label}</Typography>
+                    </Box>
+                  </MenuItem>
+                ))}
+              </Menu>
+            </Box>
+          </Toolbar>
+        </Container>
+      </AppBar>
+
+      <MainPageTable />
+    </>
   );
 }
 
