6bbd0872cbe98d7267c7d72a4268fc134c6a70e3
abisheksivaraj <abisheksivaraj2001.com>
2025-05-27 15:56:58 +0530

    add

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,8 +2,52 @@ const express = require("express");
 const cors = require("cors");
 
 const app = express();
-app.use(express.json());
-app.use(cors());
+
+
+
+
+
+
+// Increase the JSON payload size limit to 50MB
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
+
+app.use(
+  cors({
+      // origin: "https://casual-lit-tees.onrender.com",
+      origin:"http://localhost:5173",
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  })
+);
+
+// If you actually have static files on this server that need serving
+// If not, you can remove this block
+app.use(
+  express.static("public", {
+    setHeaders: (res, path) => {
+      if (path.endsWith(".css")) {
+        res.setHeader("Content-Type", "text/css");
+      }
+      if (path.endsWith(".jpg") || path.endsWith(".jpeg")) {
+        res.setHeader("Content-Type", "image/jpeg");
+      }
+      if (path.endsWith(".png")) {
+        res.setHeader("Content-Type", "image/png");
+      }
+    },
+  })
+);
+
+
+
+
+
+
+
+
+
 
 const login = require("./Route/LoginRoute");
 app.use(login);
