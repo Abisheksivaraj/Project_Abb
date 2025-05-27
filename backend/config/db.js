ce6eddfce922100673acd7d3db43d36bc32ea93b
abisheksivaraj <abisheksivaraj2001.com>
2025-05-27 16:08:47 +0530

    add

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,8 +1,12 @@
 const mongoose = require("mongoose");
-require("dotenv").config({ path: "./backend/.env" });
+
+
 const connectDb = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI); // This is undefined
+    // Use process.env.MONGO_URI to access environment variable
+    const mongoUri ="mongodb://localhost:27017/Project";
+
+    await mongoose.connect(mongoUri);
     console.log("MongoDB connected successfully");
   } catch (error) {
     console.error("MongoDB connection error:", error);
