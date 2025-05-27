abd95ef0192a634e170e0fc1e48f9ed84bb57a00
abisheksivaraj <abisheksivaraj2001.com>
2025-05-27 16:25:45 +0530

    newww

--- backend/config/db.js
+++ backend/config/db.js
@@ -7,7 +7,7 @@ const connectDb = async () => {
     const mongoUri = process.env.MONGO_URI || "mongodb://localhost:27017/Project";
     
     await mongoose.connect(mongoUri);
-    console.log("MongoDB connected successfully");
+    console.log("MongoDB connected successfully" + mongoUri);
   } catch (error) {
     console.error("MongoDB connection error:", error);
     process.exit(1);
